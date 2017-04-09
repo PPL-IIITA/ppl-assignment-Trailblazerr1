a63283a2cea30843a9607fbffb4c9d24ad8eb825
Anurag kushwaha <kanurag078@gmail.com>
2017-04-09 18:20:00 +0000

    Minor bug fixes

--- ppl_assign/q2/happify.py
+++ ppl_assign/q2/happify.py
@@ -12,22 +12,33 @@ def happ():
         giftd = json.load(ba)
     girld = initdata.datag
 
-    for i in range(0,10):
-        b = randint(0, 20)
-        g = randint(0, 50)
-        gft = randint(0, 40)
-       # girl = valueg['girl']
-       # print("b"+str(b)+' has to give '+ "gift"+str(gft)+" to g"+str(g))
-        #print("\t O guy! ask for a return gift")
-    for i in range(0, 10):
-        bb = randint(0, 20)
-        gg = randint(0, 50)
-        gft = randint(0, 40)
-        print("b"+str(bb)+" and "+"g"+str(gg)+"are happiest")
-        who_got_fooled("b"+str(bb)+" and "+"g"+str(gg)+"are happiest")
-    for i in range(0, 10):
-        bb = randint(0, 20)
-        gg = randint(0, 50)
-        gft = randint(0, 40)
-        print("b"+str(bb)+" and "+"g"+str(gg)+"are compatible")
-        who_got_fooled("b"+str(bb)+" and "+"g"+str(gg)+"are compatible")
+    for keyb, valueb in boyd.items():
+        if(valueb['commited']==True):
+            mygirl = valueb['girl']
+            for keyg, valueg in girld.items():
+                if(keyb==mygirl):
+                    mygtyp = valueg['type']
+                    mygmain = valueg['maintainanace']
+                    mygintel = valueg['intel']
+            if(valueb['type']=='miser'):
+
+    #
+    # for i in range(0,10):
+    #     b = randint(0, 20)
+    #     g = randint(0, 50)
+    #     gft = randint(0, 40)
+    #    # girl = valueg['girl']
+    #    # print("b"+str(b)+' has to give '+ "gift"+str(gft)+" to g"+str(g))
+    #     #print("\t O guy! ask for a return gift")
+    # for i in range(0, 10):
+    #     bb = randint(0, 20)
+    #     gg = randint(0, 50)
+    #     gft = randint(0, 40)
+    #     print("b"+str(bb)+" and "+"g"+str(gg)+"are happiest")
+    #     who_got_fooled("b"+str(bb)+" and "+"g"+str(gg)+"are happiest")
+    # for i in range(0, 10):
+    #     bb = randint(0, 20)
+    #     gg = randint(0, 50)
+    #     gft = randint(0, 40)
+    #     print("b"+str(bb)+" and "+"g"+str(gg)+"are compatible")
+    #     who_got_fooled("b"+str(bb)+" and "+"g"+str(gg)+"are compatible")
--- ppl_assign/q2/main2.py
+++ ppl_assign/q2/main2.py
@@ -11,27 +11,28 @@ def main2def():
     ke = datat.keys()
     sort_key = sorted(ke, key=lambda ip: datat[ip]['price'], reverse=False)
     #print(datat[sort_key[0]])
+    i = 0
     for keyg, valueg in boyd.items():
         if(valueg['commited']=="true"):
             if(valueg['type']=="miser"):
                 girl = valueg['girl']
                 i = 0
-                sum=0
+                sum = 0
                 while(sum<=girld[girl]['maintainanace'] and i<40):
                     if (datat[sort_key[i]]['from'] == " "):
                         sum = sum + datat[sort_key[i]]['price']
                         if(sum<girld[girl]['maintainanace']):
                             datat[sort_key[i]]['from']=keyg
                             datat[sort_key[i]]['to']=girl
-                            print(keyg+' has to give '+ sort_key[i]+" to "+girl)
-                            print("O guy! ask for a return gift")
-                            i+=1;
+                          #  print(keyg+' has to give '+ sort_key[i]+" to "+girl)
+                         #   print("O guy! ask for a return gift")
+                            i+=1
                         else:
                             break
                     else:
                         i+=1
                 valueg['total']=sum
-                break
+  #              print(valueg['total'])
             elif(valueg['type']=="geek"):
                 girl = valueg['girl']
                 i = 0
@@ -42,18 +43,17 @@ def main2def():
                         if(sum<girld[girl]['maintainanace']):
                             datat[sort_key[i]]['from']=keyg
                             datat[sort_key[i]]['to']=girl
-                            i+=1;
+                            i+=1
                         else:
                             break
                     else:
                         i+=1
                 valueg['total']=sum
-                break
             elif(valueg['type']=="generous"):
                 girl = valueg['girl']
                 i = 0
                 sum=0
-                while(sum<=valueg["budget"]):
+                while(sum<=valueg["budget"] and i<40):
                     if (datat[sort_key[i]]['from'] == " "):
                         sum = sum + datat[sort_key[i]]['price']
                         if(sum<valueg["budget"]):
@@ -61,12 +61,15 @@ def main2def():
                             datat[sort_key[i]]['to']=girl
                           #  print(keyg+' has to give '+ sort_key[i]+" to "+girl)
                            # print("O guy! ask for a return gift")
-                            i+=1;
+                            i+=1
                         else:
                             break
                     else:
                         i+=1
                 valueg['total']=sum
-                break
+#                print(datat[sort_key[11]]['to'])
+    with open('boydata.json', 'w') as f:
+        json.dump(boyd, f)
+        print("done")
 
 happ()
